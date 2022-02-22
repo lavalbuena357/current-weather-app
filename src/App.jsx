0f9b66f311a9c1bb0236517feab70b2ae8d0bfe1
lavalbuena357 <cuervo357@gmail.com>
2022-02-22 13:22:46 -0500

    rutas configuradas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Detail from './Pages/Detail/Detail'
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
     <div>
-      App
+      <Routes>
+        <Route path='/'  element={<Home />}/>
+        <Route path='/:id' element={<Detail />} />
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <Provider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
