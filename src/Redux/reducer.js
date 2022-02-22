dc2443d7a98ed46c8848b128432da89096af75cd
lavalbuena357 <cuervo357@gmail.com>
2022-02-22 14:43:40 -0500

    fix reducer

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -2,9 +2,13 @@ const initialState = {
   cities: []
 }
 
-export default function reducer(state = initialState, action) {
+function reducer(state = initialState, action) {
   switch(action.type) {
     case "GET_CITY":
       return {...state, cities: [...state.cities, action.payload]}
+    default:
+      return state
   }
 }
+
+export default  reducer
