e1d828e50d678b3b5dede95cdb9ec05fd572b0f6
lavalbuena357 <cuervo357@gmail.com>
2022-02-22 15:11:43 -0500

    componente de barra de busqueda

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { getCityByName } from '../../Redux/actions'
 
 function SearchBar() {
+
+  const dispatch = useDispatch()
+  const cityRef = useRef()
+
+  function handleAdd(e) {
+    e.preventDefault()
+    dispatch(getCityByName(cityRef.current.value))
+  }
+  
   return (
-    <div>SearchBar</div>
+    <div>
+      <form onSubmit={handleAdd}>
+        <input ref={cityRef} type="text" placeholder='Buscar ciudad por nombre...' />
+        <input type="submit" value="Agregar" />
+      </form>
+    </div>
   )
 }
 
