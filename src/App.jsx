e0667f95287034ebffda499ddd08845c6508f2be
lavalbuena357 <cuervo357@gmail.com>
2022-02-22 13:29:42 -0500

    modulos iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Detail from './Pages/Detail/Detail'
 import Home from './Pages/Home/Home'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/'  element={<Home />}/>
         <Route path='/:id' element={<Detail />} />
