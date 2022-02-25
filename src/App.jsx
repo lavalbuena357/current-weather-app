9a217c7023d80d6fdf3ce2706fe73231bb2774a5
lavalbuena357 <cuervo357@gmail.com>
2022-02-25 16:07:32 -0500

    app estilos

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { Routes, Route } from 'react-router-dom'
 import Detail from './Pages/Detail/Detail'
 import Home from './Pages/Home/Home'
 import Header from './Components/Header/Header'
+import style from './App.module.css'
 
 function App() {
   return (
-    <div>
+    <div className={style.container}>
       <Header />
       <Routes>
         <Route path='/'  element={<Home />}/>
