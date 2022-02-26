bcbaa3ff76c271f36d9048d9ab1b6548339f0bc8
lavalbuena357 <cuervo357@gmail.com>
2022-02-26 12:06:50 -0500

    card module - clases e iconos

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,37 +1,40 @@
 import React from 'react'
-import { IoWater, IoCloud } from "react-icons/io5";
+import { IoWater, IoCloud, IoTrash } from "react-icons/io5"
+import { MdReadMore } from "react-icons/md"
+import style from './Card.module.css'
 
 function Card(props) {
 
   return (
-    <div>
-      <div onClick={props.handleClose}>
-          x
+    <div className={style.container}>
+      <div onClick={props.handleClose} className={style.closeCtn}>
+        <IoTrash />
       </div>
-      <div>
+      <div className={style.imgCtn}>
         <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt='' />
       </div>
-      <div>
+      <div className={style.midItemsCtn}>
         <div>
           <span>{props.country} - {props.city}</span>
         </div>
-        <div>
+        <div className={style.midItem}>
           <span>{Math.round(props.temp)}°</span>
           <span>{props.weather}</span>
         </div>
       </div>  
-      <div>
-        <div>
+      <div className={style.lastItemsCtn}>
+        <div className={style.lastItem}>
           <IoWater />
           <span>{props.humidity}%</span>
         </div>
-        <div>
+        <div className={style.lastItem}>
           <IoCloud />
           <span>{props.clouds}%</span>
         </div>
       </div>
-      <div onClick={props.handleDetail}>det</div>
-    
+      <div onClick={props.handleDetail} className={style.detailCtn}>
+        <MdReadMore />
+      </div>
     </div>
   )
 }
