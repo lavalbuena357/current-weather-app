485065044232ac9159a25e5f7fa03f6325cf5dcf
lavalbuena357 <cuervo357@gmail.com>
2022-02-26 12:56:54 -0500

    header styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import style from './Header.module.css'
 
 function Header() {
   return (
     <div className={style.container}>
-      <h1>Clima Mundial</h1>
+      <Link to='/'>
+        <h1>Clima Mundial</h1>
+      </Link>
       <h2>Consulta el clima por ciudades en todo el mundo</h2>
     </div>
   )
